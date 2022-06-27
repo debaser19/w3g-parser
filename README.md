9564262f6cdff52004b5e4af631ed60defb5cbb4
debaser19 <steve@stoveld.xyz>
2022-06-27 15:45:40 -0400

    init

---
+++ README.md
@@ -0,0 +1,17 @@
+# w3g-parser
+This is a simple script that uses w3gjs to parse a w3g file and rename LastReplay.w3g.
+
+Sample output: `Shocker#21685[U]+Grubby#1278[O]+Teddster#1274[O]+GodLikeSoul#2607[O]_VS_ToD#2792[H]+BiGToBi#21514[U]+FChampRaRa#1204[H]+edholm#21389[U].w3g`
+
+## Requirements
+* nodejs
+* w3gjs
+
+`npm install w3gjs`
+
+## Usage
+### Windows
+Create a scheduled task to run every minute
+
+### Mac OS
+Create a cron job to run every minute
---
+++ w3g.js
@@ -0,0 +1,46 @@
+const { default: W3GReplay } = require("w3gjs");
+const fs = require("fs");
+const parser = new W3GReplay();
+const path = (".");    // set this to your w3g file path
+parser
+  .parse(path + "/LastReplay.w3g")
+  .then((result) => {
+    var team_0 = [];
+    var team_1 = [];
+    for (let p = 0; p < result.players.length; p++) {
+        if (result.players[p].teamid == 0) {
+            team_0.push({playerName: result.players[p].name, playerRace: result.players[p].raceDetected})
+        } else if (result.players[p].teamid == 1) {
+            team_1.push({playerName: result.players[p].name, playerRace: result.players[p].raceDetected})
+        }
+    }
+
+    var team_0_string = "";
+    var team_1_string = "";
+    for (let i = 0; i < team_0.length; i++) {
+        team_0_string += team_0[i].playerName + "[" + team_0[i].playerRace + "]";
+        if (i < team_0.length - 1) {
+            team_0_string += "+";
+        }
+    }
+
+    for (let i = 0; i < team_1.length; i++) {
+        team_1_string += team_1[i].playerName + "[" + team_1[i].playerRace + "]";
+        if (i < team_1.length - 1) {
+            team_1_string += "+";
+        }
+    }
+
+    var full_string = team_0_string + "_VS_" + team_1_string;
+    console.log(full_string)
+
+    // rename LastReplay.w3g to full_string.w3g
+    fs.rename("./LastReplay.w3g", "./" + full_string + ".w3g", function(err) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log("Replay renamed to " + full_string + ".w3g");
+        }
+    });
+  })
+  .catch(console.error);
